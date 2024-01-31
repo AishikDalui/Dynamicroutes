1fb682415ee74ce64a765f8c3deadf5aa5f000ed
Debsishu Pal <pauldebsishu2002@gmail.com>
2024-01-31 16:06:28 +0530

    Fix data fetch

--- src/data.js
+++ src/data.js
@@ -1,9 +1,8 @@
 export const student = [
+  { name: "A", roll: 2 },
+  { name: "B", roll: 3 },
+  { name: "C", roll: 4 },
+  { name: "D", roll: 5 },
+  { name: "E", roll: 6 },
+];
 
-    { name: "A", roll: 2 },
-    { name: "B", roll: 3 },
-    { name: "C", roll: 4 },
-
-    { name: "D", roll: 5 },
-    { name: "E", roll: 6 }
-]
